38a85d9c33d5fd8b8e4455fee27a80b1f0e780a8
tarunkumar <tarunkotian10@gmail.com>
2026-03-02 14:19:17 +0530

    add pkg

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'py_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tarun',
+    maintainer_email='tarun@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'paterya = py_pkg.pub:main',
+            'kenya = py_pkg.sub:main',
+        ],
+    },
+)
